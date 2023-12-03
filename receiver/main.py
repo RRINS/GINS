9b22b4c4266509820b638b39c5c820965da4ee47
Aaron Hoffman <ajh269@uakron.edu>
2023-12-03 06:36:41 +0000

    Main loop python TCP test

--- receiver/main.py
+++ receiver/main.py
@@ -2,8 +2,8 @@ import time
 import serial
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("169.254.57.66", 9000))
+HOST = "169.254.26.252"
+PORT = 9001
 
 ser = serial.Serial(
     port='/dev/ttyS0',
@@ -11,16 +11,23 @@ ser = serial.Serial(
     timeout=1,
 )
 
-try:
-    while True:
-        data = ser.readline()
-        if data:
-            print(f"Received: {data}")
-            s.send(data)
-        else:
-            s.send(b"Nothin");
-    
-except KeyboardInterrupt:
-    print("Done.")
-    ser.close()
-    s.close()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.bind((HOST, PORT))
+	while True:
+		s.listen()
+		conn, addr = s.accept()
+		print("Setup done")
+		with conn:
+			print(f"Connected by {addr}")
+            while True:
+                data = ser.readline()
+                if data:
+                    print(f"Received: {data}")
+                    conn.send(data)
+                else:
+                    conn.send(b"Nothin");
+                        
+                clientData = conn.recv(1024)
+                if clientData != "":
+                    conn.sendall(clientData)
+                    print(clientData)
