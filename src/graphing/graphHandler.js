3d0b37b4635d653df834c9a9e16417045a8b9bce
Aaron Hoffman <ajh269@uakron.edu>
2023-12-03 16:05:57 -0500

    Style acceleration graph

--- src/graphing/graphHandler.js
+++ src/graphing/graphHandler.js
@@ -4,12 +4,14 @@ let data = [
   [0, 0, 0, 0]
 ];
 
+const defaultColors = ["#FF4136", "#2ECC40", "#0074D9", "#FF851B", "#FFDC00", "#39CCCC", "#F012BE", "#b94b80", "#3D9970", "#1e7b1e", "#FF6F61", "#6b6ece", "#7FDBFF", "#B10DC9", "#FF4136", "#01FF70"];
+
 let options = {
-  xlabel: 'Time',
-  ylabel: 'Acceleration (g)'
+  colors: defaultColors,
 };
 
-const XWIDTH = 6 * 1000;
+
+const XWIDTH = 2;
 let XMin = -1;
 
 let graph = null;
@@ -21,17 +23,19 @@ const createGraphs = () => {
 }
 
 const appendData = (newData) => {
+  let time = newData["time"] / 1000.0;
+
   data.push(
     [
-      newData["time"],
+      time,
       newData["accel"]["X"],
       newData["accel"]["Y"],
       newData["accel"]["Z"],
     ]
   );
 
-  XMin = newData["time"] - XWIDTH;
-  graph.updateOptions( { 'file': data, "dateWindow": [XMin, newData["time"]] } );
+  XMin = time - XWIDTH;
+  graph.updateOptions( { 'file': data, "dateWindow": [XMin, time] } );
 }
 
 module.exports = {
